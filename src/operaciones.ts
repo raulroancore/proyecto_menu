b84d562a03ed64df8fe9c5fb7c0b11a28e28e5f0
raulroancore <raulroancore@gmail.com>
2022-07-06 16:16:47 +0200

    PRIMER COMIT MENU EN TEATRO

---
+++ src/operaciones.ts
@@ -0,0 +1,3 @@
+export function sumar(numero1, numero2) {
+  return numero1 + numero2;
+}
---
+++ src/teatro.ts
@@ -0,0 +1,22 @@
+import { Localidad } from "./modelos/localidades"
+
+export class Teatro {
+    verProgramacionActual() {
+        console.log('📅 MIÉRCOLES 5 / 07 / 2022 --- HOY')
+        console.log('')
+        console.log('')
+        console.log('--------- LA CENA DE LOS IDIOTAS ---------')
+        console.log('')
+        console.log('SINOPSIS: Pierre Brochant y sus amigos organizan todos los miércoles una cena que es una especie de apuesta: el que invite al idiota más extraordinario será el ganador. Una noche, Brochant está pletórico: ha encontrado una auténtica joya, un idiota integral. Se trata de François Pignon, un chupatintas del Ministerio de Finanzas con una gran pasion por las construcciones hechas a base de cerillas. Lo que Brochant ignora es que Pignon es un auténtico gafe, un maestro en el arte de provocar catástrofes.')
+        console.log('')
+        console.log('------------------------------------------')
+        console.log('')
+        console.log('🎭 GÉNERO: COMEDIA')
+        console.log('')
+        console.log('⏱ DURACIÓN: 95 min')
+        console.log('')
+        console.log('📍 UBICACIÓN: C/SOL, 45, SEVILLA')
+        console.log('')
+        console.log('💶 PRECIO: 30 €')
+    }
+}
